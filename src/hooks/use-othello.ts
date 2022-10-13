f856fa9efec5760bfb4e4f38ea9a6c198e69f2ca
ebina <ebina.engr@gmail.com>
2022-10-14 03:41:45 +0900

    ひっくり返す機能完成

--- src/hooks/use-othello.ts
+++ src/hooks/use-othello.ts
@@ -42,8 +42,11 @@ const othelloReducer = (state: State, action: Action): State => {
   switch (action.type) {
     case 'update':
       try {
-        move(state.board, action.fieldId, state.color)
-        return state
+        return {
+          turn: state.turn++,
+          board: move(state.board, action.fieldId, state.color),
+          color: flip(state.color)
+        }
       } catch (e) {
         console.log(e);
         return state;
@@ -59,13 +62,42 @@ const othelloReducer = (state: State, action: Action): State => {
   }
 };
 
+const directions = [
+  {row:-1, col:-1},
+  {row:-1, col: 0},
+  {row:-1, col: 1},
+  {row: 0, col:-1},
+  {row: 0, col: 1},
+  {row: 1, col:-1},
+  {row: 1, col: 0},
+  {row: 1, col: 1},
+];
+
 const move = (board: BoardData, fieldId: number, color: ColorCode) => {
   if (board[fieldId] !== EMPTY_CODE) throw Error('置けないよ！');
-  const score = getLines(board, fieldId).map((line) => {
-    return flipStoneInLine(line, color);
-  }).reduce((sum, score) => sum + score, 0)
-  console.log(score);
+
+  const scores = getLines(board, fieldId).map((line) => {
+    return countFlipableStoneInLine(line, color);
+  })
+  if (scores.reduce((sum, score) => {return sum + score}, 0) === 0) throw Error('ひとつも返せないよ！');
+  return flipStone(board, fieldId, scores, color);
 }
+
+const flipStone = (board: BoardData, fieldId: number, scores: number[], color: ColorCode) => {
+  let matrix = toMatrix(board, 8);
+  const origin = { row: Math.floor(fieldId / 8), col: fieldId % 8 };
+
+  matrix[origin.row][origin.col] = color;
+
+  directions.forEach((direction, index) => {
+    for (let offset = 1; offset <= scores[index]; offset++) {
+      let currentCoord = getCurrentCoord(origin, direction, offset);
+      matrix[currentCoord.row][currentCoord.col] = color;
+    }
+  });
+  return matrix.flat();
+}
+
 
 const toMatrix = (board: BoardData, rowLength: number): FieldObject[][] => {
     let matrix = [];
@@ -78,32 +110,36 @@ const toMatrix = (board: BoardData, rowLength: number): FieldObject[][] => {
 // FIXME: 迷走したやっつけ実装なのでリファクタリング
 const getLines = (board: BoardData, fieldId: number) => {
   const matrix = toMatrix(board, 8);
-  const coord = { row: Math.floor(fieldId / 8), col: fieldId % 8 };
+  const origin = { row: Math.floor(fieldId / 8), col: fieldId % 8 };
   
-  let lines = [];
-  for (let row_i = -1; row_i <= 1; row_i++) {
-    for (let col_j = -1; col_j <= 1; col_j++) {
-      if (row_i === 0 && col_j === 0) continue; 
-      
-      let tmp: FieldObject[] = [];
-      let offset = 1
-      while (
-        coord.row + row_i * offset >= 0 && coord.row + row_i * offset < 8 &&
-        coord.col + col_j * offset >= 0 && coord.col + col_j * offset < 8
-      ) {
-        let value =
-          matrix[coord.row + row_i * offset][coord.col + col_j * offset];
-        tmp.push(value)
-        offset++;
-      }
-      lines.push(tmp);
+  return directions.map((direction) => {
+    let tmp: FieldObject[] = [];
+    let offset = 1
+    
+    let currentCoord = getCurrentCoord(origin, direction, offset);
+    while (
+      currentCoord.row >= 0 &&
+      currentCoord.row < 8 &&
+      currentCoord.col >= 0 &&
+      currentCoord.col < 8
+    ) {
+      tmp.push(matrix[currentCoord.row][currentCoord.col]);
+      offset++;
+      currentCoord = getCurrentCoord(origin, direction, offset);
     }
-  }
-
-  return lines;
+  
+    return tmp
+  })
 }
 
-const flipStoneInLine = (line: BoardData, color: ColorCode) => {
+const getCurrentCoord = (origin: any, direction: any, offset: number) => {
+  return {
+    row: origin.row + direction.row * offset,
+    col: origin.col + direction.col * offset,
+  };
+};
+
+const countFlipableStoneInLine = (line: BoardData, color: ColorCode) => {
   if (line.length === 0) return 0;
 
   let score = 0
