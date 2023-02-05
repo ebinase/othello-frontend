0db5bb01d4406cf7d374f067f79fb114429b381d
ebina <ebina.engr@gmail.com>
2023-02-06 01:18:28 +0900

    enabled animation mode as default

--- src/components/parts/field.tsx
+++ src/components/parts/field.tsx
@@ -10,17 +10,16 @@ export type FieldObject = ColorCode | EmptyCode;
 type Props = {
   fieldId: number;
   content: ColorCode;
-  dispatcher: OthelloDispatcher;
+  dispatcher: OthelloDispatcher|undefined;
 };
 
 // TODO: メモ化
 const Field: React.FC<Props> = (props) => {
+  const dispatch = props.dispatcher;
   return (
     <div
       className="bg-slate-200 rounded-sm shadow-x2s flex items-center justify-center"
-      onClick={() => {
-        props.dispatcher({ type: "draw", position: props.fieldId });
-      }}
+      onClick={!props.content && !!dispatch ? () => dispatch({ type: "update", fieldId: props.fieldId }) : undefined}
     >
       {props.content ? <Stone color={props.content} /> : null}
     </div>
--- src/components/views/playground.tsx
+++ src/components/views/playground.tsx
@@ -3,11 +3,11 @@ import Board from "../parts/board";
 import useAnimation from "../../hooks/animation/use-animation";
 
 const PlayGround: React.FC = () => {
-  const [state, dispatch] = useOthello();
+  // const [state, dispatch] = useOthello();
   const [frame] = useAnimation();
   return (
     <div className="h-full w-full p-10 flex flex-1 justify-center flex-col items-center">
-      <Board board={frame} dispatch={dispatch} />
+      <Board board={frame} dispatch={undefined} />
     </div>
   );
 };
