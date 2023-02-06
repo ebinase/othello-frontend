df4b75557819d58f411809898bf2c7898ad46ba8
ebina <ebina.engr@gmail.com>
2023-02-06 21:58:27 +0900

    implemented play button

--- src/components/views/playground.tsx
+++ src/components/views/playground.tsx
@@ -1,14 +1,22 @@
 import useOthello from "../../hooks/use-othello";
 import Board from "../parts/board";
 import useAnimation from "../../hooks/animation/use-animation";
+import { useState } from "react";
 
 const PlayGround: React.FC = () => {
-  // const [state, dispatch] = useOthello();
+  const [state, dispatch] = useOthello();
   const [frame] = useAnimation();
+  const [isPlaying, setMode] = useState(false);
+
   return (
-    <div className="h-full w-full p-10 flex flex-1 justify-center flex-col items-center">
-      <Board board={frame} dispatch={undefined} />
-    </div>
+    <>
+      <div className="h-full w-full p-10 flex flex-1 justify-center flex-col items-center">
+        <Board board={isPlaying ? state.board : frame} dispatch={dispatch} />
+      </div>
+      <div className="h-4">
+        {!isPlaying ? <button onClick={() => setMode(!isPlaying)} className="text-slate-500">play</button> : undefined}
+      </div>
+    </>
   );
 };
 
