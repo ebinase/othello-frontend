b47719c193615c7d20b16113619fcd5625f81558
ebina <ebina.engr@gmail.com>
2023-02-10 00:47:18 +0900

    add information panel

--- src/components/parts/stone.tsx
+++ src/components/parts/stone.tsx
@@ -11,15 +11,17 @@ export const flip: any = (color: any) => {
 
 type Props = {
   color: ColorCode;
+  size?: string
 };
 
 const Stone: React.FC<Props> = (props) => {
   const color: string = props.color === 1 ? "bg-slate-50" : "bg-slate-900";
+  const size = props.size?? "h-5/6 w-5/6"
 
   return (
     <div
       className={
-        "rounded-full h-5/6 w-5/6 shadow-[2px_2px_1px_#bebebe] " + color
+        `rounded-full shadow-[2px_2px_1px_#bebebe] ${color} ${size}`
       }
     />
   );
---
+++ src/components/views/TopPanel.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Stone, { COLOR_CODES } from "../parts/stone";
+import { countStone } from "../../hooks/othello/logic/analyze";
+import { GameState } from "../../hooks/othello/useOthello";
+
+const TopPanel: React.FC<{ state: GameState }> = (props) => {
+  const state = props.state;
+  if (!state.isOver) return (<></>);
+
+  return (
+    // TODO: コンテナのサイズを固定する
+    <div className="text-slate-500 p-4">
+      <div className="font-semibold">Game Over</div>
+      <div className="flex justify-center">
+          <Stone color={COLOR_CODES.WHITE } size="h-4 w-4" />
+          <Stone color={COLOR_CODES.BLACK} size="h-4 w-4" />
+      </div>
+      <div className="flex justify-center">
+        {countStone(state.board, COLOR_CODES.WHITE)}
+        <div>-</div>
+        {countStone(state.board, COLOR_CODES.BLACK)}
+      </div>
+    </div>
+  );
+};
+
+export default TopPanel;
