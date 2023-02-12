064f861f31461ba9b302d5adba5ae6d8e83606ae
ebina <ebina.engr@gmail.com>
2023-02-12 15:59:28 +0900

    fixed bot behavior at last turn

--- src/components/views/playground.tsx
+++ src/components/views/playground.tsx
@@ -12,7 +12,7 @@ const PlayGround: React.FC = () => {
   const [calculate, changeBot] = useBot();
 
   useEffect(() => {
-    if (state.color === COLOR_CODES.WHITE) return;
+    if (state.isOver) return;
     const timeoutId = setTimeout(() => {
       if (shoudSkip(state.board, state.color)) {
         dispatch({ type: "skip" });
