56811acb78fc6adfca9d694119cc834da12165d7
ebina <ebina.engr@gmail.com>
2023-02-19 18:39:09 +0900

    選択可能なマス計算のロジックを移動

--- src/hooks/bot/bots/RandomBot.ts
+++ src/hooks/bot/bots/RandomBot.ts
@@ -1,15 +1,11 @@
 import { BoardData } from "../../../components/parts/board";
 import { ColorCode } from "../../../components/parts/stone";
-import { scoreMap } from "../../othello/logic/analyze";
+import { selectableFields } from "../../othello/logic/analyze";
 import { Calculator } from "../BotTypes";
 
 export const randomBot: Calculator = (board: BoardData, color: ColorCode) => {
-  const selectableFields: number[] = scoreMap(board, color)
-    .map((value, index) => (value > 0 ? index : undefined))
-    .filter(
-      (value): value is Exclude<typeof value, undefined> => value !== undefined
-    );
-  const i = Math.floor(Math.random() * selectableFields.length);
+ const fields = selectableFields(board, color);
+ const i = Math.floor(Math.random() * fields.length);
 
-  return selectableFields[i];
+ return fields[i];
 };
--- src/hooks/othello/logic/analyze.ts
+++ src/hooks/othello/logic/analyze.ts
@@ -7,6 +7,14 @@ export const shoudSkip = (board: BoardData, color: ColorCode): boolean => {
   return scoreMap(board, color).filter((score) => score > 0).length === 0;
 };
 
+export const selectableFields = (board: BoardData, color: ColorCode): number[] => {
+  return scoreMap(board, color)
+    .map((value, index) => (value > 0 ? index : undefined))
+    .filter(
+      (value): value is Exclude<typeof value, undefined> => value !== undefined
+    );
+};
+
 export const scoreMap = (board: BoardData, color: ColorCode) => {
   return board.map((_, fieldId) => getScore(board, color)(fieldId));
 };
