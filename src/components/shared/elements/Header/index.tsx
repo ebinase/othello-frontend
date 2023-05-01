0de2114ba9c7349d1db3b81700cd797bebf531f8
ebina <ebina.engr@gmail.com>
2023-05-02 01:41:41 +0900

    [add] header, footerをコンポーネントに切り出した

---
+++ src/components/shared/elements/Header/index.tsx
@@ -0,0 +1,12 @@
+const Header: React.FC = () => {
+  return (
+    <header>
+      <div className="h-12 w-[38rem] mx-auto flex justify-between items-center">
+        <h1 className="font-bold text-slate-500 text-xl">Othello</h1>
+        <div></div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
