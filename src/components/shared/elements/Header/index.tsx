1f2b95688c3151f796d57cd550d764b53d5aad4e
ebina <ebina.engr@gmail.com>
2023-05-02 18:21:14 +0900

    [update] HeaderにNewGameボタンを追加した

--- src/components/shared/elements/Header/index.tsx
+++ src/components/shared/elements/Header/index.tsx
@@ -1,9 +1,27 @@
+"use client";
+
 const Header: React.FC = () => {
+  const handleNewGame = () => {
+    if (confirm("プレイ中のゲームの情報が失われます。よろしいですか？")) {
+      // FIXME: リロードではなく、ゲームの状態をリセットする
+      window.location.reload();
+    }
+  };
+
   return (
     <header>
-      <div className="h-12 w-[38rem] mx-auto flex justify-between items-center">
-        <h1 className="font-bold text-slate-500 text-xl">Othello</h1>
-        <div></div>
+      <div className="h-12 mx-[2rem] flex justify-between items-center">
+        <h1 className="font-bold text-slate-500 tracking-wide text-xl">
+          Othello
+        </h1>
+        <div className="flex justify-end gap-5">
+          <button
+            onClick={handleNewGame}
+            className="text-slate-600 text-sm font-light "
+          >
+            new game
+          </button>
+        </div>
       </div>
     </header>
   );
