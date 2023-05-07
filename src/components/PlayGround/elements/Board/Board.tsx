fcf21fc54dd93ff6564edfefc989643e8927f4e7
ebina <ebina.engr@gmail.com>
2023-05-07 19:12:17 +0900

    [update] 盤面関連の処理でrecoilを利用するようにした

--- src/components/PlayGround/elements/Board/Board.tsx
+++ src/components/PlayGround/elements/Board/Board.tsx
@@ -1,23 +1,15 @@
-import { OthelloDispatcher } from "../../hooks/useOthello";
+import useOthello from "../../../../dataflow/othello/othello";
 import Field, { FieldObject } from "./Field";
 
 export type BoardData = Array<FieldObject>;
 
-type Props = { board: BoardData; dispatch?: OthelloDispatcher };
-
-const Board: React.FC<Props> = (props) => {
+const Board: React.FC = () => {
+  const { state } = useOthello();
   return (
     <>
       <div className="sm:w-96 sm:h-96 w-[90vmin] h-[90vmin] max-h-96 max-w-[24rem] bg-slate-200 grid grid-cols-8 gap-1 p-2 rounded-lg shadow-[5px_5px_5px_#bebebe,-5px_-5px_5px_#ffffff]">
-        {props.board.map((content: any, index: number) => {
-          return (
-            <Field
-              key={index}
-              fieldId={index}
-              content={content}
-              dispatcher={props?.dispatch}
-            />
-          );
+        {state.board.map((content: any, index: number) => {
+          return <Field key={index} fieldId={index} content={content} />;
         })}
       </div>
     </>
