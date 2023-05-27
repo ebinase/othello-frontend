48168ade0284caf71b91129f5678de63fb64925e
ebina <ebina.engr@gmail.com>
2023-05-28 00:02:40 +0900

    [add] ルートページにThemeProviderを追加

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,22 @@ import type { NextPage } from "next";
 import PlayGround from "../src/components/PlayGround/PlayGround";
 import Footer from "../src/components/shared/elements/Footer/Footer";
 import Header from "../src/components/shared/elements/Header/Header";
+import { ThemeProvider } from "@material-tailwind/react";
 
 const Home: NextPage = () => {
   return (
     <>
-      <div className="h-screen flex flex-col">
-        <Header />
-        <main className="flex-grow">
-          <div className="h-full flex justify-between items-center">
-            <PlayGround />
-          </div>
-        </main>
-      </div>
-      <Footer />
+      <ThemeProvider>
+        <div className="h-screen flex flex-col">
+          <Header />
+          <main className="flex-grow">
+            <div className="h-full flex justify-between items-center">
+              <PlayGround />
+            </div>
+          </main>
+        </div>
+        <Footer />
+      </ThemeProvider>
     </>
   );
 };
