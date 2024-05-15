1bb4b6971d8b37a029e5bc3dd50fc378c09a7084
ebina <ebina.engr@gmail.com>
2024-05-16 00:54:57 +0900

    [delete] tailwindのアニメーションを削除

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,25 +11,6 @@ module.exports = {
         x2s: "0.5px 0.5px 1px 0 #bebebe,-.5px -.5px 1px #ffffff",
         xs: "1px 1px 5px 0 #bebebe,-2px -2px 10px #ffffff",
       },
-      animation: {
-        "flip-in": "flip-in 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
-      },
-      keyframes: {
-        "flip-in": {
-          "0%": {
-            transform: "rotateY(-80deg);",
-            opacity: "0",
-          },
-          "50%": {
-            transform: "rotateY(-20deg);",
-            opacity: "1",
-          },
-          to: {
-            transform: "rotateY(0)",
-            opacity: "1",
-          },
-        },
-      },
     },
   },
   plugins: [],
