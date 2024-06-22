812af33be6eaaebc514d075004f42a501d379736
ebina <ebina.engr@gmail.com>
2024-06-22 19:46:50 +0900

    MCTSボットの実装をBoardクラスを利用したものに変更

--- src/components/shared/hooks/bot/methods/MCTS.ts
+++ src/components/shared/hooks/bot/methods/MCTS.ts
@@ -1,7 +1,6 @@
-import { Board, BoardData, EMPTY_CODE } from "@models/Board/Board";
+import { Board, BoardData } from "@models/Board/Board";
 import { ColorCode, flip } from "../../../../PlayGround/elements/Board/Stone";
 import { randomBot } from "./Random";
-import { directions, getCurrentCoord, getLines, toMatrix } from "@dataflow/othello/logic/matrix";
 import { COMPARISON_RESULT } from "@models/Shared/Comparison";
 
 type Options = {
@@ -13,20 +12,24 @@ export const MCTS = (board: BoardData, color: ColorCode) => {
     maxPlayOut: 500,
   };
 
-  // 選択する候補となるフィールドのリスト
-  const fields = Board.fromArray(board).selectableFields(color);
+  const baseBoard = Board.fromArray(board);
 
-  if (fields.length === 0) return null;
+  // 選択する候補となるフィールドのリスト
+  const selectableFields = baseBoard.selectableFields(color);
+
+  if (selectableFields.length === 0) return null;
 
   // ひとつのフィールドに対し何回プレイアウトを行うか決定
-  const eachPlayOutCount = Math.floor(options.maxPlayOut / fields.length);
+  const eachPlayOutCount = Math.floor(
+    options.maxPlayOut / selectableFields.length
+  );
 
   const start = new Date();
 
   // プレイアウトの結果を評価する
-  const playOutResults = fields.reduce(
+  const playOutResults = selectableFields.reduce(
     (acc, field) => {
-      const score = evaluateMove(board, color, field, eachPlayOutCount);
+      const score = evaluateMove(baseBoard, color, field, eachPlayOutCount);
       return score > acc.score ? { field, score } : acc;
     },
     { field: -1, score: -1 }
@@ -40,7 +43,7 @@ export const MCTS = (board: BoardData, color: ColorCode) => {
 
 // プレイアウトの結果を評価する
 const evaluateMove = (
-  board: BoardData,
+  board: Board,
   color: ColorCode,
   field: number,
   playOutCount: number
@@ -53,86 +56,64 @@ const evaluateMove = (
   return winCount / playOutCount;
 };
 
+/**
+ * 交互にランダムに最後まで打ち合い、勝利したかどうかを返す
+ * @param baseBoard 
+ * @param myColor 
+ * @param targetField 
+ * @returns 
+ */
 const playOut = (
-  baseBoard: BoardData,
+  baseBoard: Board,
   myColor: ColorCode,
   targetField: number
 ): boolean => {
   // まずは指定されたフィールドに石を置く
-  let board = move(baseBoard, targetField, myColor);
-  // 相手の手番からシミュレーション開始
-  let color = flip(myColor);
-  let skipCount = 0;
-  try {
-    while (!Board.fromArray(board).isFulfilled()){
-      const result = randomBot(board, color);
-      if (result !== null) {
-        board = move(board, result, color);
-        skipCount = 0;
-      } else {
-        skipCount++;
-      }
-      color = flip(color);
-      if (skipCount > 1) {
-        throw new Error('パスが連続しています');
-      }
-    }
-  
-    
-    return (
-      Board.fromArray(board).compareToOpponent(myColor) ===
-      COMPARISON_RESULT.GREATER
-    );
-  } catch (e) {
-    // 勝敗がつかない場合は負け扱いにする
-    return false;
-  }
-};
-
-// FIXME: 将来的にはBoardクラスで置き換えたいが、Botのロジックが複雑なため旧core.tsの内容を移植した
-const move = (board: BoardData, fieldId: number, color: ColorCode) => {
-  if (board[fieldId] !== EMPTY_CODE) throw Error('置けないよ！');
-
-  const scores = getLines(board, fieldId).map((line) => {
-    return countFlipableStoneInLine(line, color);
+  const result = baseBoard.update(targetField, myColor);
+  return result.when({
+    // 相手の手番からシミュレーション開始し、最終的に自分の色が多かったら勝ち
+    success: (board) => randomWalk(board, flip(myColor), 0) === myColor,
+    // 石を置けなかった場合は負け扱いする(起こり得ない)
+    failure: () => false,
   });
-  if (
-    scores.reduce((sum, score) => {
-      return sum + score;
-    }, 0) === 0
-  )
-    throw Error('ひとつも返せないよ！');
-  return flipStone(board, fieldId, scores, color);
 };
 
-const flipStone = (
-  board: BoardData,
-  fieldId: number,
-  scores: number[],
-  color: ColorCode
-) => {
-  let matrix = toMatrix(board, 8);
-  const origin = { row: Math.floor(fieldId / 8), col: fieldId % 8 };
-
-  matrix[origin.row][origin.col] = color;
-
-  directions.forEach((direction, index) => {
-    for (let offset = 1; offset <= scores[index]; offset++) {
-      let currentCoord = getCurrentCoord(origin, direction, offset);
-      matrix[currentCoord.row][currentCoord.col] = color;
-    }
-  });
-  return matrix.flat();
-};
-
-const countFlipableStoneInLine = (line: BoardData, color: ColorCode) => {
-  if (line.length === 0) return 0;
-
-  let score = 0;
-  for (let i = 0; i < line.length; i++) {
-    if (line[i] === EMPTY_CODE) return 0;
-    if (line[i] === color) return score;
-    score++;
+/**
+ * ランダムに石を打ち続け、最終的に数が多かった色を返す
+ * @param board 
+ * @param color 
+ * @param skipCount 
+ * @returns 
+ */
+const randomWalk = (board: Board, color: ColorCode, skipCount: number): ColorCode | undefined => {
+  // 盤面が埋まっている場合は勝敗を判定する
+  if (board.isFulfilled()) {
+    return judgeWinner(board, color);
   }
-  return 0;
+
+  // パスが連続した場合は引き分けなので勝者なし
+  if (skipCount > 1) {
+    return undefined;
+  }
+
+  // ランダムに石を置く場所を決定
+  const randomMove = randomBot(board.toArray(), color);
+
+  // 石を置く
+  return board.update(randomMove ?? -1, color)
+    .when({
+      success: (newBoard) => randomWalk(newBoard, flip(color), 0),  // 盤面の更新に成功したらスキップカウントをリセットして相手の手番へ
+      failure: () => randomWalk(board, flip(color), skipCount + 1), // 盤面の更新に失敗したらスキップカウントを増やして相手の手番へ
+    });
+}
+
+const judgeWinner = (board: Board, color: ColorCode): ColorCode | undefined => {
+  switch (board.compareToOpponent(color)) {
+    case COMPARISON_RESULT.EQUAL:
+      return undefined;
+    case COMPARISON_RESULT.GREATER:
+      return color;
+    case COMPARISON_RESULT.LESS:
+      return flip(color);
+  }
 };
