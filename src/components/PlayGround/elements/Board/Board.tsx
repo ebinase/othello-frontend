34a22f885cdfbf0bf92a38dc26c026dd84ca96d6
ebina <ebina.engr@gmail.com>
2024-07-06 19:28:46 +0900

    OTHELLO-45: コンポーネントではメタデータを参照するようにした

--- src/components/PlayGround/elements/Board/Board.tsx
+++ src/components/PlayGround/elements/Board/Board.tsx
@@ -2,8 +2,8 @@ import useOthello from "../../../../dataflow/othello/othello";
 import Field from "./Field";
 
 const Board: React.FC = () => {
-  const { state, getAnalysis } = useOthello();
-  const { selectableFields } = getAnalysis();
+  const { state } = useOthello();
+  const selectableFields = state.meta.active.selectable;
   return (
     <>
       <div className="sm:w-96 sm:h-96 w-[90vmin] h-[90vmin] max-h-96 max-w-[24rem] bg-slate-200 grid grid-cols-8 gap-1 p-2 rounded-lg shadow-[5px_5px_5px_#bebebe,-5px_-5px_5px_#ffffff]">
