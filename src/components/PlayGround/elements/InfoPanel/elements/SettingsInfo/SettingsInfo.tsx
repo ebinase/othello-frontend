080cda523a0c5a59cd0309cc43d5efe586c97765
ebina <ebina.engr@gmail.com>
2024-07-22 00:44:01 +0900

    tmp: 対戦相手をスクロールで選択できるようにした

--- src/components/PlayGround/elements/InfoPanel/elements/SettingsInfo/SettingsInfo.tsx
+++ src/components/PlayGround/elements/InfoPanel/elements/SettingsInfo/SettingsInfo.tsx
@@ -27,22 +27,36 @@ const SettingsInfo: React.FC = (props) => {
       <p className="text-xs text-slate-400 h-4">
         対戦相手を選択してください
       </p>
-      <div className="mt-8 flex gap-5 justify-center w-full">
-          <button
-            className="shadow-[5px_5px_5px_#bebebe,-5px_-5px_5px_#ffffff] text-slate-700 text-sm font-bold hover:text-slate-400 rounded-lg px-5 py-2 border-2 border-slate-200 w-2/5 flex gap-2 justify-center"
+      <div className="mt-8 flex gap-5 justify-start items-center py-3 px-10 w-[100vw] overflow-x-scroll">
+        <button
+            className=" shadow-[5px_5px_5px_#bebebe,-5px_-5px_5px_#ffffff] text-slate-700 text-sm font-bold hover:text-slate-400 rounded-lg px-5 py-2 border-2 border-slate-200 flex gap-2 justify-center flex-shrink-0 basis-36"
             onClick={handlePvP}
         >
           <Image src="/icon_pvp.svg" alt="settings icon" width={20} height={20} />
             <p>対人戦</p>
         </button>
-        
         <button
-          className="shadow-[5px_5px_5px_#bebebe,-5px_-5px_5px_#ffffff] text-slate-700 text-sm font-bold hover:text-slate-400 rounded-lg px-5 py-2 border-2 border-slate-200 w-2/5 flex gap-2 justify-center"
+          className="shadow-[5px_5px_5px_#bebebe,-5px_-5px_5px_#ffffff] text-slate-700 text-sm font-bold hover:text-slate-400 rounded-lg px-5 py-2 border-2 border-slate-200 flex gap-2 justify-center flex-shrink-0 basis-36"
           onClick={handlePvE}
         >
            <Image src="/icon_bot.svg" alt="settings icon" width={20} height={20} />
-          <p>ボット</p>
+          <p>Lv.1</p>
         </button>
+        <button
+          className="shadow-[5px_5px_5px_#bebebe,-5px_-5px_5px_#ffffff] text-slate-700 text-sm font-bold hover:text-slate-400 rounded-lg px-5 py-2 border-2 border-slate-200 flex gap-2 justify-center flex-shrink-0 basis-36"
+          onClick={handlePvE}
+        >
+           <Image src="/icon_bot.svg" alt="settings icon" width={20} height={20} />
+          <p>Lv.2</p>
+        </button>
+        <button
+          className="shadow-[5px_5px_5px_#bebebe,-5px_-5px_5px_#ffffff] text-slate-700 text-sm font-bold hover:text-slate-400 rounded-lg px-5 py-2 border-2 border-slate-200 flex gap-2 justify-center flex-shrink-0 basis-36"
+          onClick={handlePvE}
+        >
+           <Image src="/icon_bot.svg" alt="settings icon" width={20} height={20} />
+          <p>Lv.3</p>
+        </button>
+        
       </div>
     </div>
   );
