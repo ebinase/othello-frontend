38143e87191a1a4490bbae57f1cc6c6b4d455c03
ebina <ebina.engr@gmail.com>
2024-07-21 23:00:48 +0900

    tmp: プレイヤー情報の表示を変更

--- src/components/PlayGround/elements/InfoPanel/elements/PlayerInfo/PlayerInfo.tsx
+++ src/components/PlayGround/elements/InfoPanel/elements/PlayerInfo/PlayerInfo.tsx
@@ -2,10 +2,11 @@
 
 import useOthello from "../../../../../../dataflow/othello/useOthello";
 import { COLOR_CODE } from "@models/Board/Color"
+import Image from "next/image";
 import PlayerBar from "./PlayerBar";
 
-const PlayerInfo: React.FC = (props) => {
-  const { state, players } = useOthello();
+const PlayerInfo: React.FC = () => {
+  const { state, players, gameMode } = useOthello();
 
   const colorText = state.color === COLOR_CODE.WHITE ? "白" : "黒";
   const theme = state.color === COLOR_CODE.WHITE ? "light" : "dark";
@@ -14,7 +15,7 @@ const PlayerInfo: React.FC = (props) => {
   const data =
     players.active.type === "human"
       ? {
-          message: colorText + "のターンです",
+          message: (gameMode === "PVP" ? colorText: "あなた") + "のターンです",
           status: state.shouldSkip
             ? "置ける場所がありません"
             : state.error?.message ?? "",
@@ -27,6 +28,7 @@ const PlayerInfo: React.FC = (props) => {
   return (
     <div className=" h-full text-center flex flex-col items-center sm:justify-center pb-6 sm:pt-6 pt-10">
       <h2 className="text-slate-600 font-bold text-xl mb-2">{data.message}</h2>
+      {state.isInitialized ? ( players.active.type === "human" ? <Image src="/icon_human.svg" alt="settings icon" width={30} height={30} /> : <Image src="/icon_bot.svg" alt="settings icon" width={30} height={30} />) : undefined}
       <p className="text-xs text-slate-400 animate-pulse h-4">{data.status}</p>
     </div>
   );
