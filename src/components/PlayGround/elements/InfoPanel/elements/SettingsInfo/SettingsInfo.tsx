3fc4f9df8fde6c1259d44456f9c5648ab3346777
ebina <ebina.engr@gmail.com>
2024-07-25 01:27:20 +0900

    OTHELLO-62: レスポンシブ対応

--- src/components/PlayGround/elements/InfoPanel/elements/SettingsInfo/SettingsInfo.tsx
+++ src/components/PlayGround/elements/InfoPanel/elements/SettingsInfo/SettingsInfo.tsx
@@ -26,7 +26,7 @@ const SettingsInfo: React.FC = () => {
       <p className="text-xs text-slate-400 h-4">
         対戦相手を選択してください
       </p>
-      <div className="mt-8 flex gap-5 justify-start items-center py-3 px-10 w-[100vw] overflow-x-scroll">
+      <div className="mt-8 flex gap-5 justify-start sm:justify-center items-center py-3 px-10 w-[100vw] overflow-x-scroll">
         <button
             className="shadow-[5px_5px_5px_#bebebe,-5px_-5px_5px_#ffffff] text-slate-700 text-sm font-bold hover:text-slate-400 rounded-lg px-5 py-2 border-2 border-slate-200 flex gap-2 justify-center flex-shrink-0 basis-36"
             onClick={handlePvP}
