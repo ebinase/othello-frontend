f150bd7c525a7b182214cd315252263a4491ed5c
ebina <ebina.engr@gmail.com>
2024-07-25 00:52:51 +0900

    OTHELLO-62: UIから対戦相手を選択できるようにした

--- src/components/PlayGround/elements/InfoPanel/elements/SettingsInfo/SettingsInfo.tsx
+++ src/components/PlayGround/elements/InfoPanel/elements/SettingsInfo/SettingsInfo.tsx
@@ -3,7 +3,6 @@
 import useOthello, {
   GAME_MODE,
 } from "../../../../../../dataflow/othello/useOthello";
-import { COLOR_CODE } from "@models/Board/Color"
 
 const SettingsInfo: React.FC = () => {
   const { initialize } = useOthello();
@@ -13,32 +12,44 @@ const SettingsInfo: React.FC = () => {
     initialize({ gameMode: GAME_MODE.PVP, players: ["Player1", "Player2"] });
   };
 
-  const handlePvE = (name: string, playerColor: COLOR_CODE) => {
+  const handlePvE = (botLevel: number) => {
     initialize({
       gameMode: GAME_MODE.PVE,
-      player: name,
-      botLevel: 3,
+      player: "You",
+      botLevel,
     });
   };
 
   return (
     <div className=" h-full text-center flex flex-col items-center sm:justify-center pb-6 sm:pt-6 pt-10">
-      <h2 className="text-slate-600 font-bold text-xl mb-">NEW GAME</h2>
+      <h2 className="text-slate-600 font-bold text-xl">NEW GAME</h2>
       <p className="text-xs text-slate-400 h-4">
-        ゲームモードを選択してください
+        対戦相手を選択してください
       </p>
-      <div className="mt-6 flex flex-col gap-2">
-          <button
-            className="text-slate-700 text-sm font-bold hover:text-slate-400"
+      <div className="mt-8 flex gap-5 justify-start items-center py-3 px-10 w-[100vw] overflow-x-scroll">
+        <button
+            className="shadow-[5px_5px_5px_#bebebe,-5px_-5px_5px_#ffffff] text-slate-700 text-sm font-bold hover:text-slate-400 rounded-lg px-5 py-2 border-2 border-slate-200 flex gap-2 justify-center flex-shrink-0 basis-36"
             onClick={handlePvP}
           >
-            プレイヤー対戦
-          </button>
+            対人戦
+        </button>
         <button
-          className="text-slate-700 text-sm font-bold hover:text-slate-400"
-          onClick={() => handlePvE("Player1", COLOR_CODE.WHITE)}
+          className="shadow-[5px_5px_5px_#bebebe,-5px_-5px_5px_#ffffff] text-slate-700 text-sm font-bold hover:text-slate-400 rounded-lg px-5 py-2 border-2 border-slate-200 flex gap-2 justify-center flex-shrink-0 basis-36"
+          onClick={() => handlePvE(1)}
         >
-          ボット対戦
+          Bot Lv.1
+        </button>
+        <button
+          className="shadow-[5px_5px_5px_#bebebe,-5px_-5px_5px_#ffffff] text-slate-700 text-sm font-bold hover:text-slate-400 rounded-lg px-5 py-2 border-2 border-slate-200 flex gap-2 justify-center flex-shrink-0 basis-36"
+          onClick={() => handlePvE(2)}
+        >
+          Bot Lv.2
+        </button>
+        <button
+          className="shadow-[5px_5px_5px_#bebebe,-5px_-5px_5px_#ffffff] text-slate-700 text-sm font-bold hover:text-slate-400 rounded-lg px-5 py-2 border-2 border-slate-200 flex gap-2 justify-center flex-shrink-0 basis-36"
+          onClick={() => handlePvE(3)}
+        >
+          Bot Lv.3
         </button>
       </div>
     </div>
