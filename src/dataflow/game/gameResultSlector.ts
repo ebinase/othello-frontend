4f0df6ed5f912bf7e5e891dbaf7e150a15de3e20
ebina <ebina.engr@gmail.com>
2024-08-09 00:42:46 +0900

    OTHELLO-73: ゲームの結果を取得するslectorを実装

---
+++ src/dataflow/game/gameResultSlector.ts
@@ -0,0 +1,35 @@
+import { othelloSelector } from '@dataflow/othelloAtom';
+import { COLOR_CODE } from '@models/Board/Color';
+import { Othello } from '@models/Game/Othello';
+import { COMPARISON_RESULT } from '@models/Shared/Comparison';
+import { atom } from 'jotai';
+
+type GameResult = {
+  type: 'resulted';
+  color: COLOR_CODE;
+} | {
+  type: 'draw';
+} | null;
+
+export const gameResultSelector = atom<GameResult>((get) => {
+  const game = Othello.reconstruct(get(othelloSelector));
+
+  if (game.isOver()) {
+    switch (game.board.compareToOpponent(COLOR_CODE.WHITE)) {
+      case COMPARISON_RESULT.GREATER:
+        return {
+          type: 'resulted',
+          color: COLOR_CODE.WHITE,
+        };
+      case COMPARISON_RESULT.LESS:
+        return {
+          type: 'resulted',
+          color: COLOR_CODE.BLACK,
+        };
+      case COMPARISON_RESULT.EQUAL:
+        return { type: 'draw' };
+    }
+  } else {
+    return null;
+  }
+});
